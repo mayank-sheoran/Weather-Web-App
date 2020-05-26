faaa6978a51e47a1084d6be25bda10bfb7fbf9ef
mayank-sheoran <50839793+mayank-sheoran@users.noreply.github.com>
2020-05-27 01:12:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function subF()
 {
     var query = document.getElementById('cityInput').value;
     console.log(query);
-    const urll = "https://api.openweathermap.org/data/2.5/forecast?q=" + query + "&appid=56dea2d5f6c549faa0d2c1165f767326&units=metric#";
+    const urll = "http://api.openweathermap.org/data/2.5/forecast?q=" + query + "&appid=56dea2d5f6c549faa0d2c1165f767326&units=metric#";
     ;(async () => {
         const response = await axios({
           url: urll,
@@ -21,7 +21,7 @@ function subF()
         var lis = response.data.list;
         var lenn = lis.length;
         var last = lis[0].dt_txt
-        var iconUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+        var iconUrl = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
 
         var now = new Date();
         var days = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'];
@@ -51,7 +51,7 @@ function subF()
             var discription = response.data.list[i].weather[0].description
             var icon = response.data.list[i].weather[0].icon
             var temp = response.data.list[i].main.temp
-            var iconUrl = 'https://openweathermap.org/img/wn/' + icon + '@2x.png'
+            var iconUrl = 'http://openweathermap.org/img/wn/' + icon + '@2x.png'
             document.getElementById('java-html' + i).innerHTML+= '<h3 class="text-center dayy">'+ day.substring(0,3) +'</h3>'+
             '<img src="' + iconUrl +  '"alt="ICON" srcset="" style="width: 100%; height: 100%;">'+
             '<p class="text-center weather-disc">' + discription + '</p>'+
